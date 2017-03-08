17c06c1a938058c598636b2c9ecd27f1eca2618a
Charly MARTIN <charly.martin.1207@gmail.com>
2017-03-08 11:33:32 +0100

    Halfway through

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,9 +1,1 @@
-const player = document.querySelector('.player');
-const video = player.querySelector('video');
-const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider');
 
-const a = 2;
